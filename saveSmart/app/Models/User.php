af70fefdc053e7bd9031f09d8752897bf9e96c42
anouar el barry <elbarryanwar37@gmail.com>
2025-03-06 15:13:43 +0000

    Update User model with new attributes

--- saveSmart/app/Models/User.php
+++ saveSmart/app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\DB;
 
 class User extends Authenticatable
 {
@@ -42,4 +43,16 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+
+    protected static function boot(){
+        parent::boot();
+        static::created(function($user){
+            DB::table('totalbalance')->insert([
+                'amount' => 0,
+                'user_id' => $user->id,
+                'created_at' => now()
+            ]);
+    });
+}
 }
